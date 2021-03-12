f5b9b953d44e6e4afcea5b44cb787757e9ccbbb6
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-03-12 13:41:43 +0300

    Added component Value

---
+++ CityGuideSPA/src/app/Value/Value.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Value',
+  templateUrl: './Value.component.html',
+  styleUrls: ['./Value.component.css']
+})
+export class ValueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
